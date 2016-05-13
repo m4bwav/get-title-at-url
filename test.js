61598a2dc82b3f4142c3f3ea7e6aaf545c90ba92
m4bwav <m4bwav@gmail.com>
2016-05-13 12:47:00 -0500

    Fix test to pass.

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 import test from 'ava';
-import execa from 'execa';
-import pkg from './package.json';
 import getTitleAtUrl from './';
 
 global.Promise = Promise;
 
-test('support help shortcut', function (t) {
-    getTitleAtUrl("http://www.google.com", function (title){
-        t.assert(title === "Google");
-        t.end();  
-    })
+test.cb('support help shortcut', function (t) {
+  getTitleAtUrl('http://www.google.com', function (title) {
+    t.is(title, 'Google');
+    t.end();
+  });
 });
