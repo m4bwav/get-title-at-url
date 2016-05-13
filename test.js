ce055d9ab347feac1b69ba0c7437ff35b05b8b6c
m4bwav <m4bwav@gmail.com>
2016-05-12 23:59:29 -0500

    Added a test and testing setup.

---
+++ test.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import indentString from 'indent-string';
+import execa from 'execa';
+import pkg from './package.json';
+import getTitleAtUrl from './';
+
+global.Promise = Promise;
+
+test('support help shortcut', function (t) {
+    getTitleAtUrl("http://www.google.com", function(title){
+        t.assert(title === "Google");
+        t.end();  
+    })
+});
