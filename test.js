e39ed77314309e3272bea40efdf9beea8cf60fc3
m4bwav <m4bwav@gmail.com>
2016-05-13 23:11:05 -0500

    Fixing error tests.

--- test.js
+++ test.js
@@ -10,11 +10,18 @@ test.cb('support help shortcut', function (t) {
   });
 });
 
-test('Won\'t work with an invalid url', function () {
-  getTitleAtUrl('afewaefaefwf', function () {
-  });
+test('Won\'t work with an invalid url', function (t) {
+  try {
+    getTitleAtUrl('afewaefaefwf', function () { });
+  } catch (exception) {
+    t.pass();
+  }
 });
 
-test('Won\'t work without a callback', function () {
-  getTitleAtUrl('afewaefaefwf');
+test('Won\'t work without a callback', function (t) {
+  try {
+    getTitleAtUrl('afewaefaefwf');
+  } catch (exception) {
+    t.pass();
+  }
 });
